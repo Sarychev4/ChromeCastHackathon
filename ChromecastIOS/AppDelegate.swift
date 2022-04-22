6da218cab37bdf60856b81b16fcd2c802603677b
Sarychev4 <Sarychevas4@gmail.com>
2022-04-22 17:59:14 +0400

    Поменял рутовый контроллер.

--- ChromecastIOS/AppDelegate.swift
+++ ChromecastIOS/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        let viewController = MediaViewController()//MainViewController()
+        let viewController = MainViewController()
         let navigationController = UINavigationContainer(rootViewController: viewController)
         
         window = UIWindow(frame: UIScreen.main.bounds)
