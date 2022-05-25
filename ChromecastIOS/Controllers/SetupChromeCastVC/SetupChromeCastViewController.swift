5d666bf4f4065eab650b86bb5000b55ef9f46de8
Sarychev4 <Sarychevas4@gmail.com>
2022-05-25 20:10:02 +0400

    Add SetupChromeCastVC. Layout fixes.

---
+++ ChromecastIOS/Controllers/SetupChromeCastVC/SetupChromeCastViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SetupChromeCastViewController.swift
+//  ChromecastIOS
+//
+//  Created by Artsiom Sarychau on 25.05.2022.
+//
+
+import UIKit
+
+class SetupChromeCastViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
